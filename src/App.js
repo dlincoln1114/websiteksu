7a0a5e5858531f132e5337d281cf68180c64f81f
dlincoln1114 <dlincoln1114@yahoo.com>
2024-12-09 00:03:57 -0500

    v5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:3001/message');
+      const response = await axios.get('http://localhost:3001/api/post');
       setData(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
